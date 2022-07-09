e5975cab649e8d55c3682e5d81c5bdd17fbc499d
mzaini30 <muhzaini30@gmail.com>
2022-07-09 19:17:03 +0800

    simpan dulu

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+import {ref} from "vue"
+
+type Data = {
+	id: number;
+	sudah: boolean;
+	isi: string;
+}
+
+const data: Ref<Data[]> = ref([
+	{
+		id: Math.random(),
+		sudah: false,
+		isi: ""
+	}
+])
+</script>
+
 <template>
-  <p>Hello world</p>
+	<div class="w-full min-h-screen bg-slate-800 text-white">
+ 	 <p>Hello world</p>
+ 	</div>
 </template>
